0f9bc1fec9fbd51220edcc53d0747b4fc8d14e12
jonkaric <jonkaric@live.co.uk>
2025-04-29 16:24:45 +0100

    Remove PostCSS

--- stubs/vite.config.js
+++ stubs/vite.config.js
@@ -1,11 +1,13 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
+import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
     build: {
         sourcemap: true
     },
     plugins: [
+        tailwindcss(),
         laravel({
             input: [
                 'resources/css/site.css',
