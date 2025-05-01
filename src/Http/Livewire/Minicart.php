2435d4174d80262fd44897d4e625707f6e3fb04b
Nick <nick@arrowtide.com>
2025-05-01 12:09:32 +0100

    Escaper added to the discount code

--- src/Http/Livewire/Minicart.php
+++ src/Http/Livewire/Minicart.php
@@ -89,8 +89,10 @@ class Minicart extends Component
     {
         $this->validate();
 
+        $this->discountCode = e($this->discountCode);
+
         if (in_array($this->discountCode, $this->cartDiscountCodes)) {
-            $this->addError('discountCode', "The code '{$this->discountCode}' has already been applied.");
+            $this->addError('discountCode', "The code '({$this->discountCode}' has already been applied.");
             $this->discountCode = '';
 
             return;
